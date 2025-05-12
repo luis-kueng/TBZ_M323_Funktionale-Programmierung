0aa6280f5cfc8561d33b49abfa7d4022aa9bd3b2
Luis Küng <kuelui@proton.me>
2025-05-12 10:10:34 +0200

    feat: add aufgabe 1 and nix shell

---
+++ 01-Einfuehrung/01/main.scala
@@ -0,0 +1,25 @@
+//> using scala 3.7.0
+
+object Main
+{
+  def main(args:Array[String]): Unit =
+    println("Imperative: " + calculateScore("imperative"))
+    println("Declarative: " + wordScore("imperative"))
+
+
+  def calculateScore(word:String) : Int = {
+    var score:Int = 0
+
+    for (c <- word) {
+      if (c != 'a') {
+        score = score + 1
+      }
+    }
+
+    return score
+  }
+
+  def wordScore(word:String) : Int = {
+    return word.replace("a", "").size
+  }
+}
